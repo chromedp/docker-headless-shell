492df41f08592e41d060727239cb209ee3335872
Kenneth Shaw <kenneth.shaw@knq.io>
2017-10-05 11:16:34 +0700

    Updating build-chrome.sh script

--- build-chrome.sh
+++ build-chrome.sh
@@ -3,17 +3,29 @@
 SRC=${1:-/media/src/chromium/src}
 VER=$2
 
+set -e
+
+DEPOT_TOOLS_DIR=$(dirname $(which gclient))
+if [ -z "$DEPOT_TOOLS_DIR" ]; then
+  echo "cannot find gclient"
+  exit 1
+fi
+
+pushd $DEPOT_TOOLS_DIR &> /dev/null
+git reset --hard && git pull
+popd &> /dev/null
+
 FILES="headless/lib/headless_crash_reporter_client.cc headless/public/headless_browser.cc"
 
 pushd $SRC &> /dev/null
-
-set -ex
 
 for f in $FILES; do
   git checkout $f
 done
 
-git rebase-update
+set -x
+
+git checkout master && git rebase-update
 
 if [ -z "$VER" ]; then
   VER=$(git tag -l|grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'|sort -r -V|head -1)
