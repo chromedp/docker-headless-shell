35e7cfed3973fcf37b35646c8007b6e3b2023e83
Kenneth Shaw <kenshaw@gmail.com>
2024-04-27 16:24:12 +0700

    Adding podman-image-prune service

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # sudo loginctl enable-linger $USER
 # systemctl enable --now --user headless-shell.timer
+# systemctl enable --now --user podman-image-prune.timer
 
 SRC=$(realpath $(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd))
 
@@ -9,6 +10,6 @@ set -e
 
 (set -x;
   mkdir -p $HOME/.config/systemd/user
-  cp $SRC/headless-shell.{service,timer} $HOME/.config/systemd/user
+  cp $SRC/*.{service,timer} $HOME/.config/systemd/user
   systemctl daemon-reload --user
 )
